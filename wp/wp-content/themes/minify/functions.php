3a4c3552ea0770a5ad300a661168f410da5efb5c
Alik Welyn <alikwelynn@gmail.com>
2023-04-18 02:18:08 -0300

    add: title and thumbanil support for posts add: classic editor plugin

--- wp/wp-content/themes/minify/functions.php
+++ wp/wp-content/themes/minify/functions.php
@@ -6,3 +6,12 @@ function register_menus() {
     register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 add_action( 'init', 'register_menus' );
+
+/*
+* Title and thumbnails
+*/
+function title_and_thumbs() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+  }
+add_action( 'after_setup_theme', 'title_and_thumbs' );
